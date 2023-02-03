707bc24607852ad8af3be1c23428bc19718434cf
daeshov <jdarahthomas@gmail.com>
2023-02-03 12:34:23 -0800

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """The BaseModel class"""
 from datetime import datetime
-import uuid
+import uuid 
+import storage_type
 
 
 class BaseModel:
@@ -9,10 +10,23 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         "args won't be used"
         "Initialize the class use kwargs"
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for k in kwargs:
+                if k in ['created_at', 'updated_at']:
+                    setattr(self, k, datetime.fromisoformat(kwargs[k]))
+                elif k != '__class__':
+                    setattr(self, k, kwargs[k])
+            if storage_type == 'db':
+                if not hasattr(kwargs, 'id'):
+                    setattr(self, 'id', str(uuid.uuid4()))
+                if not hasattr(kwargs, 'created_at'):
+                    setattr(self, 'created_at', datetime.now())
+                if not hasattr(kwargs, 'updated_at'):
+                    setattr(self, 'updated_at', datetime.now())    
 
     def __str__(self):
         """Updated the print str format for the class"""
