de7ef5332ae34eb6b0c5ef7dc3a500e139779665
vigobin <vgobin@outlook.com>
2023-02-03 11:48:39 -0600

    Removed blank spaces

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ class BaseModel:
     """Defines the class BaseModel"""
     def __init__(self, *args, **kwargs):
         "Initialize the class use kwargs"
-        
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
