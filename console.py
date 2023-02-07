73d5eb633ba5fc2d281c949bbc575ea6ea8fc6c6
vigobin <vgobin@outlook.com>
2023-02-06 21:23:50 -0800

    Changed to use return statement

--- console.py
+++ console.py
@@ -13,11 +13,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, args):
         """Quit command to exit the program\n"""
-        raise SystemExit
+        return True
 
     def do_EOF(self, args):
         """EOF command to exit the program"""
-        raise SystemExit
+        return True
 
     def emptyline(self):
         """empty line + ENTER shouldn’t execute anything"""
