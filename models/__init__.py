9860e4b968bb2ae415e67c410b2ed2fd98485ab7
daeshov <jdarahthomas@gmail.com>
2023-02-07 12:27:39 -0800

    task 006

--- models/__init__.py
+++ models/__init__.py
@@ -8,6 +8,6 @@ if storage_type == 'db':
     from model.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FILeStorage
-    storage = FILeStorage()
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
     storage.reload()
