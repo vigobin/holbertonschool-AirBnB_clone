71884efbd6d6889aaa48cf7b17576f38229cde6e
vigobin <vgobin@outlook.com>
2023-02-07 21:11:33 -0600

    Corrected for pycodestyle, changed EOL to LF

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """this module instantiates an instance of the storage will be used"""
 
-from os import getenv
-
-storage_type = getenv ('HBNB_TYPE_STORAGE')
-if storage_type == 'db':
-    from model.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-    storage.reload()
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
