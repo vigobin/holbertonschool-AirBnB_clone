fc4ecca8c00d04da54bd23d9303cc70df2d409c9
vigobin <vgobin@outlook.com>
2023-02-14 14:32:38 -0800

    Added storage statement to init module

--- models/base_model.py
+++ models/base_model.py
@@ -25,6 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Updated the print str format for the class"""
